6cb67a8a5e17f18d7757aecf0668886d95d27ce0
Clément Saulle <saulle45@gmail.com>
2024-02-13 14:05:11 +0100

    equilibrage

--- JS/javascript.js
+++ JS/javascript.js
@@ -31,102 +31,111 @@ function drop(ev) {
 
 
 
-BTNFinDeTour.forEach(FinDeTour => { addEventListener('click', ecouteQuelJoueur(FinDeTour))
+BTNFinDeTour.forEach(FinDeTour => {
+  addEventListener('click', ecouteQuelJoueur(FinDeTour))
 });
-function ecouteQuelJoueur(FinDeTour){
+function ecouteQuelJoueur(FinDeTour) {
   FinDeTour.addEventListener('click', () => creerUniter(FinDeTour));
 }
 
-function creerUniter(FinDeTour){
-  if(FinDeTour.classList.contains('JA') && FinDeTourJABoolean == false){
-    Create(PionsJoueurB ,unitJB)
+function creerUniter(FinDeTour) {
+  if (FinDeTour.classList.contains('JA') && FinDeTourJABoolean == false) {
+    Create(PionsJoueurB, unitJB)
     FinDeTourJABoolean = true;
     FinDeTourJBBoolean = false;
   }
-  if(FinDeTour.classList.contains('JB') && FinDeTourJBBoolean == false){
-    Create(PionsJoueurA ,unitJA)
+  if (FinDeTour.classList.contains('JB') && FinDeTourJBBoolean == false) {
+    Create(PionsJoueurA, unitJA)
     FinDeTourJBBoolean = true;
   }
-  if(FinDeTourJBBoolean == true && FinDeTourJABoolean == true){
-  resolutionDuTour()
+  if (FinDeTourJBBoolean == true && FinDeTourJABoolean == true) {
+    resolutionDuTour()
   }
 }
 
 
 let Ligne = document.querySelectorAll('.Ligne');
-function resolutionDuTour(){
+function resolutionDuTour() {
   if (Ligne[0].hasChildNodes() === true && Ligne[4].hasChildNodes() === true) {
     let resultat1 = Ligne[0].firstChild.innerText - Ligne[4].firstChild.innerText;
-    if(resultat1<0){
+    if (resultat1 < 0) {
+      Ligne[4].firstChild.innerText = Ligne[4].firstChild.innerText - (resultat1 * -1);
       Ligne[0].firstChild.remove();
-    }else{
-      Ligne[4].firstChild.remove();    
+    } else {
+      Ligne[0].firstChild.innerText = Ligne[0].firstChild.innerText - resultat1;
+      Ligne[4].firstChild.remove();
     }
     CalculeResultat(resultat1);
     parseInt(resultat1)
   }
-   
+
   if (Ligne[1].hasChildNodes() === true && Ligne[5].hasChildNodes() === true) {
     let resultat1 = Ligne[1].firstChild.innerText - Ligne[5].firstChild.innerText;
-    if(resultat1<0){
+    if (resultat1 < 0) {
+      Ligne[5].firstChild.innerText = Ligne[5].firstChild.innerText - (resultat1 * -1);
       Ligne[1].firstChild.remove();
-      }else{
-      Ligne[5].firstChild.remove();    
-      }
+    } else {
+      Ligne[1].firstChild.innerText = Ligne[1].firstChild.innerText - resultat1;
+      Ligne[5].firstChild.remove();
+    }
     CalculeResultat(resultat1);
   }
 
-    
+
   if (Ligne[2].hasChildNodes() === true && Ligne[6].hasChildNodes() === true) {
     let resultat1 = Ligne[2].firstChild.innerText - Ligne[6].firstChild.innerText;
-    if(resultat1<0){
+    if (resultat1 < 0) {
+      Ligne[6].firstChild.innerText = Ligne[6].firstChild.innerText - (resultat1 * -1);
       Ligne[2].firstChild.remove();
-      }else{
-      Ligne[6].firstChild.remove();    
-      }
+    } else {
+      Ligne[2].firstChild.innerText = Ligne[2].firstChild.innerText - resultat1;
+      Ligne[6].firstChild.remove();
+    }
     CalculeResultat(resultat1);
 
-    }
+  }
 
-   
+
   if (Ligne[3].hasChildNodes() === true && Ligne[7].hasChildNodes() === true) {
-      let resultat1 = Ligne[3].firstChild.innerText - Ligne[7].firstChild.innerText;
-      if(resultat1<0){
-        Ligne[3].firstChild.remove();
-        }else{
-        Ligne[7].firstChild.remove();    
-      }
-      CalculeResultat(resultat1);  
+    let resultat1 = Ligne[3].firstChild.innerText - Ligne[7].firstChild.innerText;
+    if (resultat1 < 0) {
+      Ligne[7].firstChild.innerText = Ligne[7].firstChild.innerText - (resultat1 * -1);
+      Ligne[3].firstChild.remove();
+    } else {
+      Ligne[3].firstChild.innerText = Ligne[3].firstChild.innerText - resultat1;
+      Ligne[7].firstChild.remove();
     }
-    FinDeTourJABoolean = false;
-    CompteTour();
+    CalculeResultat(resultat1);
+  }
+  FinDeTourJABoolean = false;
+  CompteTour();
 }
 
 
-function CalculeResultat(resultat){
-  if(resultat > 0){
-       scoreA = resultat + scoreA;
-       document.getElementById('scoreA').textContent = scoreA;
-     }else if (resultat < 0) {
-       scoreB = resultat + scoreB;
-       document.getElementById('scoreB').textContent = scoreB * -1;
-     }
+function CalculeResultat(resultat) {
+  if (resultat > 0) {
+    scoreA = resultat + scoreA;
+    document.getElementById('scoreA').textContent = scoreA;
+  } else if (resultat < 0) {
+    scoreB = resultat + scoreB;
+    document.getElementById('scoreB').textContent = scoreB * -1;
+  }
 }
 
 let compteTour = document.getElementById('CompteTour');
 let icomptetour = 0;
-function CompteTour(){
-  
-  if(icomptetour < 5){
+function CompteTour() {
+
+  if (icomptetour < 5) {
     compteTour.innerHTML += "<div class='tour'></div>";
     icomptetour++;
-  }else{
+  } else {
     FinDeTourJABoolean = true;
     FinDeTourJBBoolean = true;
   }
 }
-compteTour.addEventListener('click',NewGame);
-function NewGame(){
+compteTour.addEventListener('click', NewGame);
+function NewGame() {
   for (let index = 0; index < Ligne.length; index++) {
     Ligne[index].every(Ligne[index].childNodes.remove);
   }
@@ -140,18 +149,18 @@ function NewGame(){
 }
 
 let number = 0;
-Create(PionsJoueurA,unitJA)
-function Create(PionsJoueur,unit){
-  if (number<30) {
-    for(i=0;i<3;i++){
+Create(PionsJoueurA, unitJA)
+function Create(PionsJoueur, unit) {
+  if (number < 30) {
+    for (i = 0; i < 3; i++) {
       let poids = unit.shift();
-      initialisationDesClass(PionsJoueur,number, poids);
+      initialisationDesClass(PionsJoueur, number, poids);
       number++;
-    } 
+    }
   }
 }
 
-function initialisationDesClass(PionsJoueur,number, poids){
+function initialisationDesClass(PionsJoueur, number, poids) {
   PionsJoueur.innerHTML += `<div id="${id}${number}" class="unite " draggable="true" ondragstart="drag(event)"><p>${poids}</p></div>`;
 }
 
@@ -160,13 +169,13 @@ function randomWithFixedSum(sum, n) {
   //Generate an array of N random numbers using Math.random(), and calculate their    sum (randomSum);
   const arr = [];
   let randomSum = 0;
-  
+
   for (let i = 0; i < n; i++) {
     arr[i] = Math.random();
     randomSum += arr[i];
   }
 
-//Scale each of the random numbers proportionally so that their sum becomes the desired sum. You can do this by multipling them by sum/randomSum, and then rounding the result. 
+  //Scale each of the random numbers proportionally so that their sum becomes the desired sum. You can do this by multipling them by sum/randomSum, and then rounding the result. 
   let prevSum = 0;
   for (let i = 0; i < n - 1; i++) {
     arr[i] = Math.round(arr[i] * sum / randomSum);
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Jeu-WarGame
+
+Objectif : Avoir plus de points que son adversaire
+
+Pour cela vous allez avoir 3 pions par tour que vous allez placez sur une des 4 colonnes.
+Une fois les pions placer, cliqué sur fin de tour.
+
+Une fois que les deux joueurs ont placés leurs pions.
+les pions face à face vont s'affronté. Le pion avec la valeur la plus forte gagne et le pion avec la valeur la moins forte est détruit.
+Le nombre de points gagner est la différence de valeur entre les deux pions.
